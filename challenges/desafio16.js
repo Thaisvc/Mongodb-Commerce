a687d36d1800ab3407d4d152967a27a3aa1fbc5b
thais viana <thaisviana081@outlook.com>
2023-01-11 10:15:51 -0300

    16 - Adicione o campo ultimaModificacao com a data corrente somente no sanduíche Big Mac
    17 - Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos

---
+++ challenges/desafio16.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany(
+    { nome: "Big Mac" },
+    { $set: { ultimaModificacao: Date() } },
+  );
+  db.produtos.find(
+    { ultimaModificacao: { $exists: true } },
+    { nome: true, _id: false },
+  );
